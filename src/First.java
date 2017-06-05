b349bf3e00ce65c4f67dbcf3a043609fe189b822
YurijZaharko <ZaharkoYurij@gmail.com>
2017-06-05 15:23:35 +0300

    2

--- src/First.java
+++ src/First.java
@@ -3,13 +3,6 @@
  */
 public class First {
     public static void main(String[] args) {
-        System.out.println("1");
-        System.out.println("1");
-        System.out.println("1");
-        System.out.println("1");
-        System.out.println("3");
-        System.out.println("3");
-        System.out.println("3");
-
+        System.out.println("2");
     }
 }
