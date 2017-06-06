d6edb88477a922021325ab84ac7f2ee5898e0b52
YurijZaharko <ZaharkoYurij@gmail.com>
2017-06-06 19:06:42 +0300

    second sout

--- src/Second.java
+++ src/Second.java
@@ -3,6 +3,12 @@ public class Second {
     public void test(){
         System.out.println("Ok");
         System.out.println("it is  Ok");
+        System.out.println("it is  Ok");
+        System.out.println("it is  Ok");
+        System.out.println("it is  Ok");
+        System.out.println("it is  Ok");
+        System.out.println("it is  Ok");
+        System.out.println("it is  Ok");
 
     }
 }
