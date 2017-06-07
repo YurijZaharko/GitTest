81e7f49f1942e7fd168c3a1fc83044a166028d27
YurijZaharko <ZaharkoYurij@gmail.com>
2017-06-07 08:25:31 +0300

    test 5

--- src/Test2.java
+++ src/Test2.java
@@ -2,4 +2,7 @@
  * Created by SC on 06.06.2017.
  */
 public class Test2 {
+    private void print(){
+        System.out.println();
+    }
 }
--- src/Test3.java
+++ src/Test3.java
@@ -2,4 +2,7 @@
  * Created by SC on 06.06.2017.
  */
 public class Test3 {
+    private void print(){
+        System.out.println();
+    }
 }
